f7762bbb011feb0e914b40d34704c188aa8a9c6c
Dmitry <babanov91@gmail.com>
2022-08-09 22:57:16 +0500

    add POST, GET

--- client.py
+++ client.py
@@ -7,6 +7,12 @@ HOST = 'http://127.0.0.1:5000'
 # print(response.status_code)
 # print(response.text)
 
-response = requests.post(f'{HOST}/test/', json={'key': 'value'}, headers={'token': 'dfge494g3'}, params={'k1': 'v1'})
+# response = requests.post(f'{HOST}/test/', json={'key': 'value'}, headers={'token': 'dfge494g3'}, params={'k1': 'v1'})
+response = requests.post(f'{HOST}/users/', json={'name': 'user_1', 'password': '1234'})
+print(response.status_code)
+print(response.text)
+
+
+response = requests.get(f'{HOST}/users/1')
 print(response.status_code)
 print(response.text)
--- server.py
+++ server.py
@@ -1,8 +1,31 @@
 # import requests
-from flask import Flask, jsonify
-from flask import request
+# from flask import request
+from flask import Flask, jsonify, request
+from flask.views import MethodView
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String, DateTime, func, create_engine
 
 app = Flask('app')
+PG_DSN = 'postgresql://app:1234@127.0.0.1/flask'
+# PG_DSN = 'postgresql://app:1234@0.0.0.0/flask'
+
+engine = create_engine(PG_DSN, pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+class User:
+
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, server_default=func.null())
+
+
+# Base.metadata.create_all(engine)
+
 
 @app.route('/test/', methods=['POST'])
 def test():
@@ -19,4 +42,42 @@ def test():
                     })
 
 
+class UserView(MethodView):
+
+    def get(self, user_id: int):
+        with Session as session:
+            user = session.query(User).get(user_id)
+            if user is None:
+                pass
+            return jsonify({'name': user.name, 'created_at': user.created_at.isoformat()})
+
+
+    def post(self):
+        json_data = request.json
+        with Session() as session:
+            user = User(name=json_data['name'], password=json_data['password'])
+            session.add(user)
+            session.commit()
+            return {'id': user.id}
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+user_view = UserView.as_view('users')
+app.add_url_rule('/users/', view_func=user_view, methods=['POST'])
+app.add_url_rule('/users/<int:user_id>', view_func=user_view, methods=['GET'])
+
 app.run()
+
+
+
+
+
+
+
+
+
